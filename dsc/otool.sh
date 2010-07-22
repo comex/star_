b242316f423c2cdbb32d0509b2408754d7767c1d
comex <comexk@gmail.com>
2010-07-21 22:51:52 -0400

    otool

--- dsc/otool.sh
+++ dsc/otool.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Usage: ./otool.sh iPad1,1_3.2.cache outpad
 set -e
+if [ "x$OTOOL" = x ]; then export OTOOL="otool -tvv"; fi
 if [ -e f/System ]; then umount f; fi
 test x"$2" != "x"
 ./dsc "$1" f
@@ -8,6 +9,6 @@ while [ ! -e f/System ]; do :; done
 find f -type f | (while IFS='*' read x
 do
 echo "$x"
-otool -tvv "$x" > "$2"/"$(basename "$x" .dylib)".txt
+$OTOOL "$x" > "$2"/"$(basename "$x" .dylib)".txt
 done)
 
