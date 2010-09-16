d44678334f5737d3b025a887ba5325d2f5b4562d
comex <comexk@gmail.com>
2010-09-16 04:01:01 +0000

    fixes

--- config/ipsw.py
+++ config/ipsw.py
@@ -15,10 +15,9 @@ atexit.register(go_away)
 
 input_path = os.path.realpath(sys.argv[1])
 os.chdir(os.path.dirname(os.path.realpath(sys.argv[0])))
-fbs = os.path.realpath('FirmwareBundles')
 configdata = os.path.realpath('configdata.py')
-iDeviceKeys = plistlib.readPlist('iDeviceKeys.plist')
 out_root = os.path.realpath('../bs')
+keyz = os.path.realpath('keyz.txt')
 tmpdir = tempfile.mkdtemp()
 print 'tmpdir:', tmpdir
 os.chdir(tmpdir)
@@ -33,8 +32,8 @@ output = os.path.join(out_root, short_identifier)
 os.mkdir(output)
 
 kc_key = kc_iv = fs_key = None
-for line in open('keyz.txt'):
-    bits = re.split(':? ', line)
+for line in open(keyz):
+    bits = re.split(':? ', line.strip())
     if bits[0] == short_identifier + '.KernelCache':
         kc_key = bits[1]
         kc_iv = bits[2]
@@ -42,57 +41,33 @@ for line in open('keyz.txt'):
         fs_key = bits[1]
 
 if kc_key is None or kc_iv is None or fs_key is None:
-    raise ValueError('Couldn\'t get keys for %s' % short_identifier)
-
-pwnage_pl_fn = '%s/%s.bundle/Info.plist' % (fbs, identifier)
-
-stuff = iDeviceKeys.get(pl['ProductType'], {}).get(pl['ProductVersion'])
-
-if stuff is not None and stuff.has_key('KernelCache'):
-    kc = stuff['KernelCache']
-    kc_key = kc['Key']
-    kc_iv = kc['IV']
-    fs_key = stuff['RootFilesystem']['RootFilesystemKey']
-elif os.path.exists(pwnage_pl_fn):
-    system('plutil -convert xml1 %s' % pwnage_pl_fn)
-    pwnage_pl = plistlib.readPlist(pwnage_pl_fn)
-    kc = pwnage_pl['FirmwarePatches']['KernelCache']
-    kc_key = kc['Key']
-    print kc
-    kc_iv = kc['IV']
-    fs_key = pwnage_pl['RootFilesystemKey']
-else:
-    print 'No keys...'
+    print 'Couldn\'t get keys for %s' % short_identifier
     sys.exit(1)
 
 print 'kernelcache...'
 kc_name = pl.get('KernelCachesByTarget', pl.get('KernelCachesByPlatform')).values()[0]['Release']
-z.extract(kc_name)
-system('xpwntool %s tempkc.e -k %s -iv %s -decrypt' % (kc_name, kc_key, kc_iv)) #!
+system('unzip -q -o -j "%s" %s' % (input_path, kc_name))
+system('~/xpwnbin/xpwntool %s tempkc.e -k %s -iv %s -decrypt' % (kc_name, kc_key, kc_iv)) #!
 os.unlink(kc_name)
-system('xpwntool tempkc.e %s/kern' % output) #!
+system('~/xpwnbin/xpwntool tempkc.e %s/kern' % output) #!
 os.unlink('tempkc.e')
 
 print 'root filesystem...'
 fs_name = pl['SystemRestoreImages']['User']
 system('unzip -q -o -j "%s" %s' % (input_path, fs_name)) # 'unzip' used for speed
-system('vfdecrypt -i %s -k %s -o temproot.dmg' % (fs_name, fs_key))
-os.mkdir('mnt')
-system('hdiutil attach -noverify -mountpoint mnt temproot.dmg')
-if os.path.exists('mnt/System/Library/Caches/com.apple.dyld/dyld_shared_cache_armv7'):
-    arch = 'armv7'
-    shutil.copy('mnt/System/Library/Caches/com.apple.dyld/dyld_shared_cache_armv7', '%s/cache' % output)
-elif os.path.exists('mnt/System/Library/Caches/com.apple.dyld/dyld_shared_cache_armv6'):
+system('~/xpwnbin/dmg extract %s temproot.img -k %s' % (fs_name, fs_key))
+try:
+    system('~/xpwnbin/hfsplus temproot.img extract /System/Library/Caches/com.apple.dyld/dyld_shared_cache_armv7 %s/cache || true' % output)
+except:
+    system('~/xpwnbin/hfsplus temproot.img extract /System/Library/Caches/com.apple.dyld/dyld_shared_cache_armv6 %s/cache || true' % output)
     arch = 'armv6'
-    shutil.copy('mnt/System/Library/Caches/com.apple.dyld/dyld_shared_cache_armv6', '%s/cache' % output)
+else:
+    arch = 'armv7'
+system('~/xpwnbin/hfsplus temproot.img extract /sbin/launchd %s/launchd' % output)
 
-shutil.copy('mnt/sbin/launchd', '%s/launchd' % output)
 os.chmod('%s/launchd' % output, 0755)
-system('hdiutil detach mnt')
-os.unlink('temproot.dmg')
+os.unlink('temproot.img')
 os.unlink(fs_name)
-p = os.popen('lipo -info %s/launchd' % output)
-stuff = p.read().strip()
 
 if '3.1.' in short_identifier:
     arch += '_3.1.x'
@@ -112,6 +87,5 @@ if not eval('{%s}' % open(configdata).read()).has_key(short_identifier):
     open(configdata, 'a').write(new + '\n')
 
 # clean up
-os.rmdir('mnt')
 os.chdir('/')
 os.rmdir(tmpdir)
