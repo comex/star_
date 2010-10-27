5b521ad84e011ae199c6d0b1237a69bee8934669
comex <comexk@gmail.com>
2010-10-27 16:48:31 -0400

    omg

--- goo/pf/transe.py
+++ goo/pf/transe.py
@@ -9,6 +9,8 @@ make_avail()
 zero = ptrI(0)
 funcall(cfg['#cache']['sysctlbyname'], ptr('security.mac.proc_enforce', True), 0, 0, zero, 4)
 funcall(cfg['#cache']['sysctlbyname'], ptr('security.mac.vnode_enforce', True), 0, 0, zero, 4)
+pf1 = ptr('./pf2', True)
+funcall(cfg['#cache']['execve'], pf1, ptrI(pf1, 0), zero)
 pf2 = ptr('/usr/lib/pf2', True)
 funcall(cfg['#cache']['execve'], pf2, ptrI(pf2, 0), zero)
 
