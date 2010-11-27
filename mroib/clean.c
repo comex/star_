da0394f9d377b16ed7420b172090faa739db528e
comex <comexk@gmail.com>
2010-11-27 17:37:53 -0500

    mroib

--- mroib/clean.c
+++ mroib/clean.c
@@ -1,6 +1,6 @@
 void CleanAndInvalidateCPUDataCache(void *buffer, int bufferLen) {
     char *p = buffer;
-    while(bufferLen) {
+    while(bufferLen > 0) {
         asm("mcr p15, 0, %0, c7, c10, 1" :: "r"(p));
         p += 0x10; // should be higher
         bufferLen -= 0x10;
