7fa7bc92b18722456a865a60463638eb0231ea88
comex <comexk@gmail.com>
2011-01-24 20:27:38 -0500

    whew.

--- locutus/inject.h
+++ locutus/inject.h
@@ -2,5 +2,6 @@
 #include <mach/mach.h>
 
 // asynchronous
-// inject the specified dylib, but also create a new port which we have a send right to and it has the receive right to, and store it in its TASK_GSSD_PORT slot
+// inject the specified dylib
+// also create a new port which we have a send right to and it has the receive right to, and store it in its TASK_GSSD_PORT slot
 kern_return_t inject(pid_t pid, const char *path, mach_port_t *gssd);
